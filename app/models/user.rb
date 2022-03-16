5ca8f3fe80a8d50bbc4a184ecb01634073aa28a4
ymokkk <yamasaan126@gmail.com>
2022-03-16 17:38:45 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :lastname_kana, presence: true
+         validates :firstname_kana, presence: true
+         validates :birthday, presence: true
 end
