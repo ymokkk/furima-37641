975192f8822c05cff4fd8e8ce6cac3a6d9f3e401
ymokkk <yamasaan126@gmail.com>
2022-03-17 14:40:54 +0900

    バリデーションの再実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,19 @@ class User < ApplicationRecord
   has_many :comments
   has_many :orders
 
-         validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :lastname_kana, presence: true
-         validates :firstname_kana, presence: true
-         validates :birthday, presence: true
+  with_options presence: true do
+         validates :nickname
+         with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."} do
+         validates :last_name
+         validates :first_name
+         end
+         with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
+         validates :lastname_kana
+         validates :firstname_kana
+         end
+         validates :birthday
+   end
 
 PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 end
