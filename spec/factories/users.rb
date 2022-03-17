f2960e5bcfc112bac709eee718907b28cccdc878
ymokkk <yamasaan126@gmail.com>
2022-03-17 20:06:46 +0900

    PasswordのFaker部分の修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :user do
     nickname {"test"}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     last_name {"山田"}
     lastname_kana {"ヤマダ"}
