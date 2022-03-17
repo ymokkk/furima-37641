f7b20b84d3f8049d3f9f2696faa64c4f7c693e88
ymokkk <yamasaan126@gmail.com>
2022-03-17 11:43:42 +0900

    エラーメッセージの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
          validates :lastname_kana, presence: true
          validates :firstname_kana, presence: true
          validates :birthday, presence: true
-         
+
 PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
