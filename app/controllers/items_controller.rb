527b8ffe12f27602b20399c452e197624494c5a8
ymokkk <yamasaan126@gmail.com>
2022-03-18 19:00:58 +0900

    バリデーションの再設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,14 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
   def create
     @item = Item.new(item_params)
-    if @item.valid?
-      @item.save
+    if @item.save
       redirect_to root_path
     else
      render :new
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,14 +11,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :explanation
+  end
 
+  with_options presence: true, numericality: { other_than: 1 , message: "can't be blank" } do
+  validates :category_id
+  validates :status_id
+  validates :delivery_id
+  validates :area_id
+  validates :shipping_day_id
+  end
 
-  validates :item_name, presence: true
-  validates :explanation, presence: true
-  validates :category_id, numericality: {other_than: 1 , message: "can't be blank"}
-  validates :status_id, numericality: {other_than: 1 , message: "can't be blank"}
-  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/, message: "Half-width number" }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
 end
