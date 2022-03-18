6bb2af1571979aa1097956afb82cba0c2a715e88
ymokkk <yamasaan126@gmail.com>
2022-03-18 12:23:01 +0900

    ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,26 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+     render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :delivery_id, :area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   has_many :comments
   has_one :order
   belongs_to :user
+
+
+  
 end
