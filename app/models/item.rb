2335a38dda23b71ddda7b4577a4930bc260ce9f8
ymokkk <yamasaan126@gmail.com>
2022-03-18 12:29:20 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,12 @@ class Item < ApplicationRecord
   belongs_to :user
 
 
-  
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_id, presence: true
+  validates :area_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
 end
