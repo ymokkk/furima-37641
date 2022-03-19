147988082e3026296a68934bc0610edaf17327a9
ymokkk <yamasaan126@gmail.com>
2022-03-19 19:46:17 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,12 +16,14 @@ class Item < ApplicationRecord
     validates :item_name
     validates :explanation
     validates :price
-    validates :price, numericality: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters." }
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
     validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
     validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
     validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank" }
     validates :area_id, numericality: { other_than: 1 , message: "can't be blank" }
     validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank" }
   end
+
+  validates :price, numericality: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters." }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  
 end
