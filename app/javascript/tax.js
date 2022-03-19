5736d9e2b08b6b503abacf7dda09a74cd609abe6
ymokkk <yamasaan126@gmail.com>
2022-03-19 15:30:16 +0900

    手数料と利益の表示実装

---
+++ app/javascript/tax.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () =>{
+  const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const addTaxPrice = document.getElementById("add-tax-price");
+      addTaxPrice.innerHTML = Math.round(priceInput.value * 0.1);
+      const profit = document.getElementById("profit");
+      profit.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1))
+    })
+});
