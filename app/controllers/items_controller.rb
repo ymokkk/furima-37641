55e4016b6402b4fa97bc198f4619a7c791fc62c6
ymokkk <yamasaan126@gmail.com>
2022-03-22 19:44:40 +0900

    コントローラーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -35,8 +35,12 @@ class ItemsController < ApplicationController
   end
   
   def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+    end
+    redirect_to root_path
   end
-  
+
   private
 
   def item_params
