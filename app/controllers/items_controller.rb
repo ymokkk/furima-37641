206c6c0c3931733e2cd48cbee3727e5242696f77
ymokkk <yamasaan126@gmail.com>
2022-03-22 17:51:31 +0900

    他ユーザーの場合の編集ページ遷移不可の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def update
