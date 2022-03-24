e0d93f9cfb81fb6c2466de616e67ef9a0f51f0a2
ymokkk <yamasaan126@gmail.com>
2022-03-24 16:59:26 +0900

    エラーメッセージの実装

--- app/models/order_mailing.rb
+++ app/models/order_mailing.rb
@@ -4,13 +4,15 @@ class OrderMailing
 
   with_options presence: true do
     validates :address_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :sichoson
     validates :banchi
     validates :phone_number
     validates :item_id
     validates :user_id
   end
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :phone_number, format: {with: /\A[0-9]+\z/, message: "is invalid. Input only number"}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message:"is too short"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
