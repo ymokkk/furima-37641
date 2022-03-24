c1cc26dcdd73be55a35ba1efd85e303f6325d8a9
ymokkk <yamasaan126@gmail.com>
2022-03-24 15:14:42 +0900

    テーブルに保存する処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+   @order_mailing = OrderMailing.new(order_params)
+   if @order_mailing.valid?
+    @order_mailing.save
+    redirect_to root_path
+   else
+    render :index
+   end
+  end
+
+private
+
+  def order_params
+    params.require(:order_mailing).permit(:address_number, :area_id, :sichoson, :banchi, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
-
--- app/models/order_mailing.rb
+++ app/models/order_mailing.rb
@@ -1,18 +1,19 @@
 class OrderMailing
   include ActiveModel::Model
-  attr_accessor :address_number, :area_id, :sichoson, :banchi, :building, :phone_number, :order
+  attr_accessor :address_number, :area_id, :sichoson, :banchi, :building, :phone_number, :item_id, :user_id
 
   with_options presence: true do
     validates :address_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :sichoson
     validates :banchi
     validates :phone_number
-    validates :order
+    validates :item_id
+    validates :user_id
   end
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    order = Order.create(user: user, item: item)
-    Mailing.create(address_number: address_number, area_id:area.id, sichoson: sichoson, banchi: banchi, building: building, phone_number: phone_number)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Mailing.create(address_number: address_number, area_id:area_id, sichoson: sichoson, banchi: banchi, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
